d237c11a32e18000707333e3ad169beab7b26317
Chaya Segal <chayale90@gmail.com>
2023-05-21 16:58:29 +0300

    formPage begin

--- client/src/components/FormComponents/FormPage.tsx
+++ client/src/components/FormComponents/FormPage.tsx
@@ -1,8 +1,18 @@
+import "./FormPage.css";
 
 const FormPage = () => {
   return (
-    <div>
-     <h1>FormPage</h1> 
+    <div className="question">
+      <img src="src/assets/process1.png" alt="process1" />
+      <div className="weHere">אנחנו כאן לשמוע את קולך!</div>
+      <div className="numberQ">שאלה 2 מתוך 5</div>
+      <div className="question">האם את מפחדת או חוששת
+        מאדם הקרוב אליך?
+      </div>
+      <div className="vxButtons">
+        <img src="src/assets/icons/VBigIcon.svg" alt="" />
+        <img src="src/assets/icons/xBigIcon.svg" alt="" />
+      </div>
     </div>
   );
 };
