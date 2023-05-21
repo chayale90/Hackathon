f74fa80997814b10a0bc418541ad2e411f734264
unknown <shmuel.cohen0@gmail.com>
2023-05-21 14:48:49 +0300

    finish with welcome-page

--- client/src/component/CustomComponents/Button.tsx
+++ client/src/component/CustomComponents/Button.tsx
@@ -5,6 +5,7 @@ export const FullButton = styled(Button)<ButtonProps>(() => ({
   backgroundColor: "#E554C2",
   color: "black",
   fontWeight: "bold",
+  fontSize: "20px",
   "&:hover": {
     backgroundColor: "var(--hover-button)",
   },
@@ -15,6 +16,8 @@ export const BorderButton = styled(Button)<ButtonProps>(() => ({
   color: "black",
   fontWeight: "bold",
   border: "1px solid #ab4292",
+  fontSize: "20px",
+
   "&:hover": {
     backgroundColor: "var(--hover-button)",
   },
