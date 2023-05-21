abe4d50e2351cb01ca973774e92e2c074d127e7f
Chaya Segal <chayale90@gmail.com>
2023-05-21 14:59:40 +0300

    form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import WelcomePage from "./component/WelcomePage/WelcomePage";
 import FormPage from "./component/FormComponents/FormPage";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="main">
         <Routes>
           <Route index element={<WelcomePage />} />
-          <Route path="/" element={<FormPage />} />
+          <Route path="/form" element={<FormPage />} />
         </Routes>
       </div>
     </BrowserRouter>
--- client/src/component/FormComponents/FormPage.tsx
+++ client/src/component/FormComponents/FormPage.tsx
@@ -1,5 +1,4 @@
 
-
 const FormPage = () => {
   return (
     <div>
