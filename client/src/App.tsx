89366e2166b1394c67109cf86420f7d4ba17cb83
Chaya Segal <chayale90@gmail.com>
2023-05-21 13:46:49 +0300

    form route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import EnterPage from "./component/EnterPage/EnterPage";
+import Form from "./component/Form/form";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="main">
         <Routes>
-          <Route path="/" element={<EnterPage />} />
+          <Route index element={<EnterPage />} />
+          <Route path="/form" element={<Form />} />
         </Routes>
       </div>
     </BrowserRouter>
