8455058f1a1327fda0bab20e71fa17945970dee3
unknown <shmuel.cohen0@gmail.com>
2023-05-22 17:59:54 +0300

    add navigate to risklevel page

--- src/components/FormComponents/FormPage.tsx
+++ src/components/FormComponents/FormPage.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Question from "./Question";
 import "./FormPage.css";
 import Header from "../Header/Header";
+import { useNavigate } from "react-router-dom";
 
 const FormPage: React.FC = () => {
   const [questionIndex, setQuestionIndex] = useState(0);
+  const navigate = useNavigate();
+  useEffect(() => {
+    questionIndex === questions.length && navigate("/riskLevel");
+    console.log(questionIndex);
+  }, [questionIndex]);
   const questions = [
     "אם את מפחדת או חוששת מאדם הקרוב אליך?",
     "האם פוגעים בך נפשית, מילולית, רגשית כלכלית, פיזית?",
@@ -30,21 +36,20 @@ const FormPage: React.FC = () => {
   };
 
   const handleX = () => {
-    resultsArr.push(false)
+    resultsArr.push(false);
     handleNextQuestion();
-    isFullArray()
+    isFullArray();
   };
 
   const handleV = () => {
-    resultsArr.push(true)
+    resultsArr.push(true);
     handleNextQuestion();
-    isFullArray()
-
+    isFullArray();
   };
 
   const isFullArray = () => {
     if (questionIndex == 4) {
-      checkResults()
+      checkResults();
     }
   };
 
@@ -52,36 +57,53 @@ const FormPage: React.FC = () => {
     // Displaying the results in the console
     if (
       // q1=true && q4=true
-      resultsArr[0] == true && resultsArr[3] == true ||
+      (resultsArr[0] == true && resultsArr[3] == true) ||
       // q1=true && q2=true
-      resultsArr[0] == true && resultsArr[1] == true ||
+      (resultsArr[0] == true && resultsArr[1] == true) ||
       // q1=true && q5=true
-      resultsArr[0] == true && resultsArr[4] == true ||
+      (resultsArr[0] == true && resultsArr[4] == true) ||
       // q4=true
       resultsArr[3] == true ||
       // q2=true
       resultsArr[1]
-    ) setLevel("high")
-    
+    )
+      setLevel("high");
     else if (
-      resultsArr[0] == true && resultsArr[1] == false && resultsArr[2] == false && resultsArr[3] == false && resultsArr[4] == false
-      ||
-      resultsArr[0] == true && resultsArr[1] == false && resultsArr[2] == true && resultsArr[3] == false && resultsArr[4] == false
-      ||
-      resultsArr[0] == false && resultsArr[1] == false && resultsArr[2] == false && resultsArr[3] == false && resultsArr[4] == true
-    ) setLevel("middle")
-
+      (resultsArr[0] == true &&
+        resultsArr[1] == false &&
+        resultsArr[2] == false &&
+        resultsArr[3] == false &&
+        resultsArr[4] == false) ||
+      (resultsArr[0] == true &&
+        resultsArr[1] == false &&
+        resultsArr[2] == true &&
+        resultsArr[3] == false &&
+        resultsArr[4] == false) ||
+      (resultsArr[0] == false &&
+        resultsArr[1] == false &&
+        resultsArr[2] == false &&
+        resultsArr[3] == false &&
+        resultsArr[4] == true)
+    )
+      setLevel("middle");
     else if (
-      resultsArr[0] == false && resultsArr[1] == false && resultsArr[2] == true && resultsArr[3] == false && resultsArr[4] == false
-      ||
-      resultsArr[0] == false && resultsArr[1] == false && resultsArr[2] == false && resultsArr[3] == false && resultsArr[4] == false
-    ) setLevel("low") 
+      (resultsArr[0] == false &&
+        resultsArr[1] == false &&
+        resultsArr[2] == true &&
+        resultsArr[3] == false &&
+        resultsArr[4] == false) ||
+      (resultsArr[0] == false &&
+        resultsArr[1] == false &&
+        resultsArr[2] == false &&
+        resultsArr[3] == false &&
+        resultsArr[4] == false)
+    )
+      setLevel("low");
 
     for (let i = 0; i < resultsArr.length; i++) {
       console.log(`q${i + 1}: ${resultsArr[i]}`);
     }
   };
-
 
   return (
     <div className="question page">
@@ -89,16 +111,9 @@ const FormPage: React.FC = () => {
       <img src={`src/assets/${processImg[questionIndex]}`} alt="process1" />
       <div className="weHere">אנחנו כאן לשמוע את קולך!</div>
       <div className="numberQ">שאלה {questionIndex + 1} מתוך 5</div>
-      {questionIndex < questions.length ? (
-        <Question
-          content={questions[questionIndex]}
-          onV={handleV}
-          onX={handleX}
-        />
-      ) : (<div>
-        <div>All questions answered!</div>
-        <h4> level:{level}</h4>
-      </div>
+      <p>{questions[questionIndex]}</p>
+      {questionIndex < questions.length && (
+        <Question onV={handleV} onX={handleX} />
       )}
     </div>
   );
--- src/components/FormComponents/Question.tsx
+++ src/components/FormComponents/Question.tsx
@@ -3,19 +3,15 @@ import VBigIcon from "../../assets/icons/VBigIcon.svg";
 import XBigIcon from "../../assets/icons/xBigIcon.svg";
 
 interface QuestionProps {
-  content: string;
   onV: () => void;
   onX: () => void;
 }
 
-const Question: React.FC<QuestionProps> = ({ content, onV, onX }) => {
+const Question: React.FC<QuestionProps> = ({ onV, onX }) => {
   return (
-    <div>
-      <p>{content}</p>
-      <div className="vxButtons">
-        <img src={VBigIcon} alt="v-icon" onClick={onV} />
-        <img src={XBigIcon} alt="x-icon" onClick={onX} />
-      </div>
+    <div className="vxButtons">
+      <img src={VBigIcon} alt="v-icon" onClick={onV} />
+      <img src={XBigIcon} alt="x-icon" onClick={onX} />
     </div>
   );
 };
