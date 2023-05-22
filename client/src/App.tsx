a52b08b39b10c0da7cb24b7e0619a03ba3e64b47
unknown <shmuel.cohen0@gmail.com>
2023-05-22 13:02:38 +0300

    add rist level page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage/WelcomePage";
 import FormPage from "./components/FormComponents/FormPage";
 import LocationPage from "./components/LocationPage/LocationPage";
+import RiskLevelPage from "./components/RiskLevelPage/RiskLevelPage";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<WelcomePage />} />
           <Route path="/location" element={<LocationPage />} />
           <Route path="/form" element={<FormPage />} />
+          <Route path="/risk-level" element={<RiskLevelPage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ client/src/components/RiskLevelPage/RiskLevelPage.tsx
@@ -0,0 +1,21 @@
+import Header from "../Header/Header";
+import "./RiskLevelPage.css";
+import { FullButton } from "../CustomComponents/Button";
+import RiskErr from "../../assets/icons/risk-error.svg";
+const RiskLevelPage = () => {
+  return (
+    <div className="page risk-level">
+      <Header />
+      <img src={RiskErr} alt="" />
+      <p>בהתאם לתשובות שלך, אנחנו מעריכים שרמת הסכנה שבה את נמצאת כעת הנה: </p>
+      <h1>רמת סכנה גבוהה</h1>
+      <p>
+        אנא ממך, אל תחכי עוד! ופני באופן מיידי לאחת העמותות שרוצות לעזור לך
+        להציל את חייך והקרובים אליך!
+      </p>
+      <FullButton>לפנייה אנונימית לעמותה באזור שלי !</FullButton>
+    </div>
+  );
+};
+
+export default RiskLevelPage;
